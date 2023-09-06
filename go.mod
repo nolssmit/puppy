7d7cf24f5182593e81852436444d8d0ee688ebe2
Nols Smit <nols.smit@gmail.com>
2023-09-06 15:03:43 +0200

    WIP

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nolssmit/puppy
 
 go 1.21.0
+
+require github.com/nolssmit/dog v0.0.0-20230904132008-953c5292569c // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,20 @@
 package puppy
 
+import (
+	"github.com/nolssmit/dog"
+)
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() {
+	return dog.WhenGrownUp(Barks())
 }
